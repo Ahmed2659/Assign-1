ec1e326c45d66fd5223eb9bf8694538a1bb1ce7f
Ahmed2659 <adenahmed393@gmail.com>
2024-10-03 19:17:12 +0300

    Add files via upload

---
+++ assingment I/Simple.c
@@ -0,0 +1,16 @@
+/*
+Author:AHMED ADEN
+Reg NO:CT102/G/24914/24
+*/
+//Declaration and initialisation
+#include <stdio.h>
+//Program to calculate simple interest
+ int main (){
+    int Principalamount=2000;
+    int time=2;
+    int rate=4;
+    int simpleinterest=(Principalamount*time*rate)/100;
+    printf("The simpleinterest is\t%d",simpleinterest);
+    return 0;
+
+ }
